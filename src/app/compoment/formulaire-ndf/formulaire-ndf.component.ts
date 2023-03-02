3eb971ad0cf31c21884e3428310fe8c0da75acdb
Corentin Duriez <corentinduriez22@gmail.com>
2023-03-02 15:47:01 +0100

    Add input file to  NdF form + Details NoF started

--- src/app/compoment/formulaire-ndf/formulaire-ndf.component.ts
+++ src/app/compoment/formulaire-ndf/formulaire-ndf.component.ts
@@ -10,6 +10,7 @@ import { HttpServiceService } from 'src/app/service/http-service.service';
 })
 export class FormulaireNdfComponent implements OnInit {
   form : FormGroup
+  myFiles : any[] =[]
   constructor(private fb : FormBuilder,
     private httpService : HttpServiceService,
     private route: Router ) { }
@@ -19,7 +20,9 @@ export class FormulaireNdfComponent implements OnInit {
       title: this.fb.control('',[Validators.required]),
       date: this.fb.control('',[Validators.required]),
       description: this.fb.control('',[Validators.required]),
-      amount: this.fb.control('',[Validators.required])
+      amount: this.fb.control('',[Validators.required]),
+      categorie: this.fb.control('',[Validators.required]),
+      image: this.fb.control('',[Validators.required])
     })
   }
    get title(){
@@ -37,10 +40,37 @@ export class FormulaireNdfComponent implements OnInit {
     return this.form.get("amount")
    }
 
-   submit(){
-    this.route.navigate(['/mesndf'])
+   get categorie(){
+    return this.form.get("categorie")
    }
-   
+
+   get image(){
+    return this.form.get("image")
+   }
+
+   onFileChange(event : any){
+    for( let i=0; i < event.target.files.length;i++){
+      this.myFiles.push(event.target.files[i])
+    }
+
+   }
+
+   submit(){
+    if(this.form.valid){
+      const formData = new FormData();
+
+      for(let i=0;i < this.myFiles.length;i++){
+        // console.log(this.myFiles[i])
+        formData.append(this.myFiles[i].name,this.myFiles[i])
+        console.log(formData.get(this.myFiles[i].name))
+      }
+
+
+    }
+    // this.route.navigate(['/mesndf'])
+    this.myFiles = []
+   }
+
    deconnect(){
     this.route.navigate(['/connexion'])
   }
