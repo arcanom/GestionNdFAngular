9bb06235c2ca0d2ddf4279c8af559d382723bcf1
Corentin Duriez <corentinduriez22@gmail.com>
2023-03-07 17:06:31 +0100

    Test token

--- src/app/compoment/connexion/connexion.component.ts
+++ src/app/compoment/connexion/connexion.component.ts
@@ -1,6 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import { FormBuilder, FormGroup, Validators } from '@angular/forms';
 import { Router } from '@angular/router';
+import { Employeelogin } from 'src/app/model/employeelogin';
 import { HttpServiceService } from 'src/app/service/http-service.service';
 import { StorageService } from 'src/app/service/storage.service';
 
@@ -11,7 +12,10 @@ import { StorageService } from 'src/app/service/storage.service';
 })
 export class ConnexionComponent implements OnInit {
 form : FormGroup
-
+employee : Employeelogin
+model : any
+data : any
+token: any
   constructor(private fb : FormBuilder,
     private route : Router,
     private storageService :  StorageService,
@@ -20,21 +24,33 @@ form : FormGroup
 
   ngOnInit(): void {
     this.form =  this.fb.group({
-      name: this.fb.control('',[Validators.required]),
-      firstName: this.fb.control('',[Validators.required]),
+      username: this.fb.control('',[Validators.required]),
       password: this.fb.control('',[Validators.required,Validators.pattern('(?=.*[a-z])(?=.*[A-Z])(?=.*[0-9])(?=.*[$@$!%*?&])[A-Za-z\d$@$!%*?&].{8,}')])
     })
   }
 
-  get userName(){
-    return this.form.get("name")
+  get username(){
+    return this.form.get("username")
   }
+
   get password(){
     return this.form.get("password")
   }
 
   connect(){
-      this.route.navigate(['/mesndf'])
+    if(this.form.valid){
+      this.employee = {
+        username : this.form.value.username,
+        password: this.form.value.password
+      }
+      this.httpService.login(this.employee).subscribe((responseData)=>{
+
+      this.data = responseData
+      this.token = this.data.token
+      console.log(this.token)
+      })
+    }
+      // this.route.navigate(['/mesndf'])
   }
 
 }
